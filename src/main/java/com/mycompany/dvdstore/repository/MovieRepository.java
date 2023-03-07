90d0df586dcfce98bec75ea4beb0e2a73335553b
D-Rudy <duriez.ry@gmail.com>
2023-03-07 19:37:10 +0100

    entity, service, repository

---
+++ src/main/java/com/mycompany/dvdstore/repository/MovieRepository.java
@@ -0,0 +1,20 @@
+package com.mycompany.dvdstore.repository;
+
+import com.mycompany.dvdstore.entity.Movie;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieRepository {
+
+    private final Logger logger = LoggerFactory.getLogger(MovieRepository.class);
+
+    private final List<Movie> movies = new ArrayList<Movie>();
+    public void add (Movie movie){
+        movies.add(movie);
+        logger.info("The movie {} added!", movie.getTitle());
+    }
+}
--- src/test/java/com/mycompany/dvdstore/AppTest.java
+++ src/test/java/com/mycompany/dvdstore/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.mycompany.dvdstore;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
