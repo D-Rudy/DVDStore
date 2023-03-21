d9531a21160669bcba2ec78db54cb8221c80628d
D-Rudy <duriez.ry@gmail.com>
2023-03-21 16:15:23 +0100

    Conteneur léger de spring

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -3,15 +3,16 @@ package com.mycompany.dvdstore;
 import com.mycompany.dvdstore.controller.MovieController;
 import com.mycompany.dvdstore.repository.FileMovieRepository;
 import com.mycompany.dvdstore.service.DefaultMovieService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
-        MovieController movieController = new MovieController();
-        FileMovieRepository movieRepository = new FileMovieRepository();
-        DefaultMovieService defaultMovieService = new DefaultMovieService();
-        movieController.setMovieService(defaultMovieService);
-        defaultMovieService.setMovieRepository(movieRepository);
+
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        MovieController movieController=context.getBean(MovieController.class);
         movieController.addUsingConsole();
     }
 }
