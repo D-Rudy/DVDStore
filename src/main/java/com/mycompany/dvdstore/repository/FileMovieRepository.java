773a5ed2dfe79853e9e316289b10e5851ab9c1fb
D-Rudy <duriez.ry@gmail.com>
2023-03-21 16:24:10 +0100

    File writer csv

--- src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
+++ src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
@@ -4,6 +4,7 @@ import com.mycompany.dvdstore.entity.Movie;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -11,11 +12,13 @@ public class FileMovieRepository implements MovieRepositoryInterface {
 
     private final Logger logger = LoggerFactory.getLogger(FileMovieRepository.class);
 
+    private File file;
+
     public void add(Movie movie) {
         logger.trace("Adding movie in a txt file");
         FileWriter writer;
         try {
-            writer = new FileWriter("movies.txt", true);
+            writer = new FileWriter(file, true);
             writer.write(movie.getGenre() + ";" + movie.getTitle() + "\n");
             writer.close();
 
@@ -25,5 +28,11 @@ public class FileMovieRepository implements MovieRepositoryInterface {
         logger.info("The movie {} added!", movie.getTitle());
     }
 
+    public File getFile() {
+        return file;
+    }
 
+    public void setFile(File file) {
+        this.file = file;
+    }
 }
