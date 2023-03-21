16abb9912daf86f5db597c9be6e685d9950a55b1
D-Rudy <duriez.ry@gmail.com>
2023-03-21 17:05:45 +0100

    suppression applicationContext.xml & Use annotations

--- src/main/java/com/mycompany/dvdstore/repository/MovieRepositoryInterface.java
+++ src/main/java/com/mycompany/dvdstore/repository/MovieRepositoryInterface.java
@@ -3,7 +3,7 @@ package com.mycompany.dvdstore.repository;
 import com.mycompany.dvdstore.entity.Movie;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface MovieRepositoryInterface {
 
     void add(Movie movie);
